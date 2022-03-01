5fc9428edab2404a1619e1653cd315e62a6abb0c
TavaresCarlos <caca_cht@hotmail.com>
2022-03-01 10:54:42 -0300

    Lendo .csv com o pandas e enviando para .html com o Flask

--- sistema/backend/server.py
+++ sistema/backend/server.py
@@ -1,8 +1,16 @@
 #pip install flask
 #pip install -U flask-cors
 
-from flask import Flask
+'''
+Flask =
+render_template =
+jsonify =
+send_file = 
+'''
+
+from flask import Flask, request, render_template, jsonify, send_file,
 from flask_cors import CORS, cross_origin
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,6 +19,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/')
 @cross_origin()
 def home():
-	return ('Hello World, cross-origin')
+	#Convertendo o .csv para .html
+	data = pd.read_csv('recursos-naturais-municipios.csv', on_bad_lines='skip')
+	return render_template('index.html', tables=[data.to_html()], titles=[''])
 
-app.run('0.0.0.0')
+app.run(host="localhost", port=int("3000"))
