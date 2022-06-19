973441798215283d8d71126a761f1638622e6771
TavaresCarlos <caca_cht@hotmail.com>
2022-06-19 11:10:22 -0300

    Updated docker compose

---
+++ sistema/frontend/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var axios = require('axios');
+
+var app = express();
+app.use(cors());
+
+app.get('/', async (req, res) => {
+	res.send('Frontend');
+	await axios.get('http://localhost:3000/display')
+	.then((res) => {
+		console.log("S")
+		console.log((res.data));
+	})
+	.catch((error) => {
+		console.log(error.response);
+	})
+});
+
+app.listen(8000)
+console.log("Servidor online")
